229764f80c4be9c10bae52af1b62d8162721ae05
oriolbc_ioc <oriol.b.c@hotmail.com>
2025-01-31 10:41:31 +0100

    correcció guest.layout i migracio taula students(abans canviat el nom directe amb sql

--- escoles/resources/views/layouts/guest.blade.php
+++ escoles/resources/views/layouts/guest.blade.php
@@ -16,12 +16,11 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            <div style="width: 200px; height: 200px;">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-full h-full fill-current text-gray-500" />
                 </a>
             </div>
-
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
